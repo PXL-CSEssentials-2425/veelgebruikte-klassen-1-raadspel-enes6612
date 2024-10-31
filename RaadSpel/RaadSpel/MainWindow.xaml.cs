ae5a41476b29b753dd00413bcfe15d7c8e3542b8
Enes Tanriverdi <Enes.Tanriverdi@student.pxl.be>
2024-10-31 13:46:38 +0100

    gemaakt

---
+++ RaadSpel/RaadSpel/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RaadSpel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int teRadenGetal;
+        int aantalPogingen;
+        Random random = new Random();
+
+        private void endButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void evaluatieButton_Click(object sender, RoutedEventArgs e)
+        {
+           if (int.TryParse(numberTextBox.Text, out int gebruikersgetal))
+            {
+                aantalPogingen++;
+
+                if (gebruikersgetal == teRadenGetal)
+                {
+                    output1TextBox.Text = "Proficiat! U hebt het getal geraden!";
+                }
+                else if (gebruikersgetal > teRadenGetal)
+                {
+                    output1TextBox.Text = "Raad lager!";
+                }
+                else if (gebruikersgetal < teRadenGetal)
+                {
+                    output1TextBox.Text = "Raad hoger!";
+                }
+                output2TextBox.Text = $"Aantal keren geraden: {aantalPogingen}";
+            }
+           else
+            {
+                output1TextBox.Text = "Voer een geldig getal in!";
+            }
+        }
+
+        private void newButton_Click(object sender, RoutedEventArgs e)
+        {
+            teRadenGetal = random.Next(1, 101);
+            aantalPogingen = 0;
+            output1TextBox.Text = "Raad het getal!";
+            output2TextBox.Text = "Aantal keren geraden: 0";
+        }
+    }
+}
